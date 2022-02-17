7a9ac99fccaf9f7a8cfd51f2b0baaefbf57b58b9
Steve Bilogan <steve.bilogan@gmail.com>
2022-02-17 10:34:36 -0500

    asd

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/CommandBar/CommandBarLine.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/CommandBar/CommandBarLine.xaml.cs
@@ -17,10 +17,11 @@ using Windows.UI.Xaml.Navigation;
 
 namespace UITests.Windows_UI_Xaml_Controls.CommandBar
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class CommandBarLine : Page
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[SampleControlInfo("CommandBar", nameof(CommandBarLine))]
+	public sealed partial class CommandBarLine : Page
     {
         public CommandBarLine()
         {
